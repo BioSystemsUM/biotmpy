752926c24b60dc3c3f254b90125d762c565c19c9
ana Lisboa <analisboasan@gmail.com>
2023-06-15 23:25:39 +0100

    pipeline

--- src/biotmpy/wrappers/europePMC_wrapper.py
+++ src/biotmpy/wrappers/europePMC_wrapper.py
@@ -18,7 +18,7 @@ import pathlib
 def pmc_to_list(term): 
     results = []
     print('getting papers')
-    url = f'https://www.ebi.ac.uk/europepmc/webservices/rest/search?query=EXT_ID:("PMC")%20AND%20(TITLE:"{term}" OR ABSTRACT:"{term}")&pageSize=1000&resultType=core&format=json' 
+    url = f'https://www.ebi.ac.uk/europepmc/webservices/rest/search?query=(TITLE:"{term}" OR ABSTRACT:"{term}")&pageSize=1000&resultType=core&format=json' 
 
     nextPage = True
     while nextPage:
@@ -42,12 +42,14 @@ def list_to_paper_instances(paper_list, keyword=None):
         abstract = paper_dict.get('abstractText', '')  
         source = paper_dict.get('source')
         paper_obj = Paper(pmid, title, abstract, source, query_keyword=keyword)
-        paper_instances.append(paper_obj)
-    #print(paper_instances)
+        if 'target' in paper_obj.title_abstract:
+            paper_instances.append(paper_obj)
+
+
     with open('paper_instances1.pickle', 'wb') as file:
         pickle.dump(paper_instances, file)
-    return paper_instances
 
+    return paper_instances
 
 
 def get_annotations_paper_instances(paper_instances):
@@ -64,12 +66,13 @@ def get_annotations(paper_instance):
     id = paper_instance.id
 
     annotations_url= f'https://www.ebi.ac.uk/europepmc/annotations_api/annotationsByArticleIds?articleIds={source}%3A{id}&pageSize=1000&type=Organisms%2CGene_Proteins%2CDiseases%2CChemicals'
-    #annotations_url= f'https://www.ebi.ac.uk/europepmc/annotations_api/annotationsByArticleIds?articleIds=PMC%3A36419223&pageSize=1000&type=Organisms%2CGene_Proteins%2CDiseases%2CChemicals'
+    
     
     try:
         response = requests.get(annotations_url)
         annotations_data = response.json()
-        paper_instance = annotations_to_dict(annotations_data[0], paper_instance)
+        if len(annotations_data)>0:
+            paper_instance = annotations_to_dict(annotations_data[0], paper_instance)
         return paper_instance
 
 
@@ -88,10 +91,13 @@ def get_annotations(paper_instance):
 def annotations_to_dict(json_anotations, paper_instance):
     for annotation in json_anotations['annotations']:
         category = annotation['type']
-        exact = annotation['exact'].lower()
-        if exact not in paper_instance.annotations[category]:
-            paper_instance.annotations[category].append(exact)
-        return paper_instance
+        exact = annotation['exact'].lower().rstrip()
+        section = annotation.get('section', '')
+        if 'Title' in section or 'Abstract' in section:
+            if exact not in paper_instance.annotations[category]:
+                paper_instance.annotations[category].append(exact)
+    return paper_instance
+
 
 
          
@@ -102,7 +108,8 @@ def pmc_to_papers(keyword):
 
 
 # if __name__=="__main__": 
-#     articles = pmc_to_list('tuberculosis')
-#     papers = list_to_paper_instances(articles)
+#    articles = pmc_to_list('tuberculosis')
+#    papers = list_to_paper_instances(articles)
+
 
     
--- src/biotmpy/wrappers/paper.py
+++ src/biotmpy/wrappers/paper.py
@@ -10,11 +10,25 @@ class Paper:
                             'Organisms':[]}
         
         self.annotations_scispacy = {'Diseases':[],
-                    'Chemicals':[],
-                    'Gene_Proteins':[],
-                    'Organisms':[]}
+                                        'Chemicals':[],
+                                        'Gene_Proteins':[],
+                                        'Organisms':[]}
+        
+     
         if query_keyword:
             self.query_keyword = query_keyword
 
         self.title_abstract = self.title + self.abstract
         
+
+    def compare_annotations(self):
+        for category in self.annotations:
+            annotations1 = set(self.annotations[category])
+            annotations2 = set(self.annotations_scispacy[category])
+            
+            if annotations1 == annotations2:
+                print(f"Annotations for '{category}' are the same.")
+            else:
+                different_annotations = annotations1.symmetric_difference(annotations2)
+                print(f"Different annotations for '{category}': {different_annotations}")
+  
